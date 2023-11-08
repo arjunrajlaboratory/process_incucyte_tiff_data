51a3c091da491be6dad60efb41f81227f6c30551
arjunrajlab <arjunrajlab@gmail.com>
2023-11-08 12:32:39 -0500

    Added dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'process-incucyte-tiff-data = process_incucyte_tiff_data.process_tiff_data:main',
+            'process-incucyte-tiff-data=process_incucyte_tiff_data.process_tiff_data:main',
         ],
     },
+    install_requires=[
+        'tifffile',  # Add 'tifffile' to the list of required packages
+        # You can specify versions as well, e.g., 'tifffile>=2021.7.2'
+        # Add any other required packages here, each as a string in this list
+    ],
+    # Include any other parameters you had before or may want to add
 )
